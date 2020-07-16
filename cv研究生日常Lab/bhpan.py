0f152d8d984767daa542ad7faa47094e4d6c6d72
ywz978020607 <ywzsunny2012@163.com>
2020-07-16 11:20:09 +0800

    up

--- cv研究生日常Lab/bhpan.py
+++ cv研究生日常Lab/bhpan.py
@@ -1,15 +1,18 @@
 #下载北航云盘的文件
 import sys
 import os
-url = sys.argv[1]
-name = sys.argv[2]
+argv_len = len(sys.argv)
+if argv_len==3:
+    url = sys.argv[1]
+    name = sys.argv[2]
+else:
+    url = input("downloads_url:")
+    name = input("filename_to_save:")
 os.system("wget -c '"+url+"' -O "+name)
 
-# def down(url,name):
-#     os.system("wget -c '"+url+"' -O "+name)
-
-
-
-
 #eg: python bhpan.py 'https://p300s.buaa.edu.cn:10002/bhpan_bucket/7f451e0dxxxx'  test.rar
+#    or
+#    python bhpan.py
+
+
 
