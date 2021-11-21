9eaf0b7fce49ac05b4eaa5389bcda1e79c738afe
ywz978020607 <ywzsunny2012@163.com>
2021-11-21 23:24:58 +0800

    update

--- 智能软硬件 (python全栈系列)/Micropython_esp32_8266/mpy-mqtt-onenet-utils/单片机esp32封装类简化/test1.py
+++ 智能软硬件 (python全栈系列)/Micropython_esp32_8266/mpy-mqtt-onenet-utils/单片机esp32封装类简化/test1.py
@@ -1,5 +1,5 @@
 from T1 import *
-
+#esp32端
 ###############################################
 CLIENT_ID = "857129375"
 username='234533'
@@ -15,10 +15,7 @@ data_name = ["temp","hum"]
 data_value = [15.5,36.5]
 
 def task_main():
-    global c,TOPIC_UP
-
     myMQTT.publish('$dp',data_name,data_value)
-
     myMQTT.c.check_msg() #检测接收并调用sub_cb
 
 def run():
