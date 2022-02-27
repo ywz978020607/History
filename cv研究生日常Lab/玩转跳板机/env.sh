c6810b9b469f4574dfe8c9dbb632d3b85dfdbbe0
ywz978020607 <ywzsunny2012@163.com>
2022-02-27 20:59:22 +0800

    add-multi-jump

--- cv研究生日常Lab/玩转跳板机/env.sh
+++ cv研究生日常Lab/玩转跳板机/env.sh
@@ -14,11 +14,15 @@ help(){
 
     #3.连接内网机器(可选)
     s518    #第一步中选择的跳板机/无跳板机进行内网机器ssh连接
+    ###多级跳板ssh[注意规则]
+    s518 ,$jump2,$jump3 #第一跳为第一步选择的跳板机 之后的跳板机输命令时按需要的顺序即可
+    ssh ywz@$ip518 -J $jump1,$jump2,$jump3 #与上一句命令效果相同(假设第一步选择的jump1)
 
     #4.映射内网机器端口(可选)
     # mapping函数主要用来端口映射，方便scp/sftp/打开浏览器查看面板等高级操作 搭配本机ip固定127.0.0.1进一步使用
-    mapping $ip518 22 9050 #将内网机器的22端口(默认 可缺省) 映射到本机的9050端口
-
+    mapping $ip518 22 9050 #将内网机器的22端口(默认ssh端口) 映射到本机的9050端口
+    ###多级跳板后映射机器端口[注意规则]
+    mapping $ip518 22 9050 $jump1,$jump2 #端口映射多级跳转规则: mapping的第四个参数为优先跳板列表, 最后再以第一步中选择的跳板机为最后一跳
 
     #5.查看配置的内网机器ip(可选)
     echo $ip518
@@ -36,11 +40,12 @@ help(){
 alias s220="ssh yangwenzhe@183.129.176.220"
 
 # -------------跳板机配置--------------
-jumpnum=4
+jumpnum=5
 jump1="ywz@467830y6j3.zicp.vip:32027" #207
 jump2="ywz@467830y6j3.zicp.vip:57009" #3090
 jump3="ywz@370581k45d.wicp.vip:57526" #509
 jump4="dyf@cn-zz-bgp-7.natfrp.cloud:14775" #ipdyf
+jump5="ywz@cn-hn-dx-1.natfrp.cloud:56603" #ipywz
 # --------跳板机直连[命令eg:>j1]-------
 for((i=1;i<=$jumpnum;i++));  
 do
@@ -72,28 +77,26 @@ ip518="10.135.6.78"
 ip509="10.134.162.159"
 ip3090="10.134.162.65"
 ipdyf="10.130.157.75"
-ipywz="10.136.150.144"
 
 # ------------------常用: 内网跳连SSH[命令eg:>s518]-----------------------
 alias s518="ssh ywz@$ip518 $jump"
 alias s509="ssh ywz@$ip509 $jump"
 alias sdyf="ssh dyf@$ipdyf $jump"
-alias sywz="ssh ywz@$ipywz $jump"
-
 
 
 # ----------常用: 跳板机端口映射[命令eg:>mapping $ip518 22 9050]----------
 # scp跳板机-将本机端口 通过跳板机映射到某个机器ip上的某个端口
-# 参数说明: mapping [内网机器-ip] [内网机器-端口-默认22可缺省] [自定义映射到本机的端口]
+# 参数说明: mapping [内网机器-ip] [内网机器-端口] [自定义映射到本机的端口]
 mapping(){
-    if [ $# == 2 ]
+    if [ $# == 3 ]
     then
-        config="0.0.0.0:$2:$1:22"
-    elif [ $# == 3 ]
+        config="0.0.0.0:$3:$1:$2" #config="0.0.0.0:$2:$1:22"
+        mapcmd="ssh -L $config ${jump/:/ -p }" #ywz@467830y6j3.zicp.vip -p 32027;
+    elif [ $# == 4 ]
     then
         config="0.0.0.0:$3:$1:$2"
+        mapcmd="ssh -L $config ${jump/:/ -p } -J $4" #ywz@467830y6j3.zicp.vip -p 32027;
     fi
-    mapcmd="ssh -L $config ${jump/:/ -p }" #ywz@467830y6j3.zicp.vip -p 32027;
     mapcmd=${mapcmd/-J/}
     echo $mapcmd
     $mapcmd
@@ -108,5 +111,8 @@ alias sync="git add -A && git commit -m 'up' && git push origin master"
 
 # 参考
 # alias j1="ssh ${jump1/:/ -p }" => ssh ywz@467830xxx3.zicp.vip -p 32xxx
-# ssh ywz@10.135.6.78 -J ywz@467830y6j3.zicp.vip:32027
+# 多级跳转
+# ssh ywz@10.135.6.78 -J ywz@467830y6j3.zicp.vip:32027 ,dyf@10.130.157.75:22
 # ssh -L 0.0.0.0:9000:10.130.159.113:22 ywz@467830y6j3.zicp.vip -p 32027
+# 多级跳转
+# ssh -L 0.0.0.0:9000:10.130.159.113:22 ywz@10.135.6.78 -p 22 -J dyf@cn-zz-bgp-7.natfrp.cloud:14775,dyf@cn-zz-bgp-7.natfrp.cloud:14775
