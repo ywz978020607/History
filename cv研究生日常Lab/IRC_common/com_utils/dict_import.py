5fb0251a9a733ad1ffafd2d505635940519c455f
ywz978020607 <ywzsunny@buaa.edu.cn>
2022-03-26 12:45:22 +0800

    up

---
+++ cv研究生日常Lab/IRC_common/com_utils/dict_import.py
@@ -0,0 +1,15 @@
+# 一键自动引入动态参数，有别于手动修改.conf或是修改parser/sys.argv
+import argparse
+
+
+def auto_import(arg_dict={}, argv_str=""):
+    # arg_dict = arg_dict or {
+    #     "database_name": "aftercut512",
+    #     "cuda_id": "0",
+    # }
+    parser = argparse.ArgumentParser()
+    for key in arg_dict:
+        parser.add_argument('--'+key.replace("_","-"), type=type(arg_dict[key]), default=arg_dict[key])
+        parser.add_argument('--'+key, type=type(arg_dict[key]), default=arg_dict[key])
+    args = parser.parse_args(argv_str)
+    return vars(args)
