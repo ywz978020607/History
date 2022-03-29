497b59e73834eb8b0876226efa212a54dde02155
ywz978020607 <ywzsunny@buaa.edu.cn>
2022-03-29 20:53:39 +0800

    up

--- cv研究生日常Lab/mydocker/env.sh
+++ cv研究生日常Lab/mydocker/env.sh
@@ -2,7 +2,8 @@
 
 alias build="docker-compose build"
 
+
+# TODO: 使用脚本，包括初次使用，退出-继续运行/停止，重入等
 alias start="docker-compose up -d"
 alias debug="docker-compose up"
-
 alias site="docker-compose run --rm --service-port ywz_cuda11_1 bash"
