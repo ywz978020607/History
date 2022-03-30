adc6415c81ced3203816826bff0b1be4bce992ff
ywz978020607 <ywzsunny@buaa.edu.cn>
2022-03-30 15:52:01 +0800

    up

--- cv研究生日常Lab/mydocker/env.sh
+++ cv研究生日常Lab/mydocker/env.sh
@@ -7,4 +7,4 @@ alias build="docker-compose build"
 # TODO: 使用脚本，包括初次使用，退出-继续运行/停止，重入等
 # alias start="docker-compose up -d"
 # alias debug="docker-compose up"
-alias site="docker-compose run --rm --service-port ldl bash"
+alias site="docker-compose run --service-port ldl bash"
