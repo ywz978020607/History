c25e95ec27ec690e565854ecfab4a0b00892e656
ywz978020607 <ywzsunny@buaa.edu.cn>
2022-04-03 23:53:14 +0800

    up

--- cv研究生日常Lab/mydocker/build.sh
+++ cv研究生日常Lab/mydocker/build.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
 apt-get update
-apt-get install -y sudo
-# apt-get install -y ssh git gcc g++ make tmux vim net-tools
-# apt-get install -y libsm6 libxext6 libxrender-dev
-# # wget https://mirrors.tuna.tsinghua.edu.cn/anaconda/archive/Anaconda3-2021.11-Linux-x86_64.sh
+apt-get install -y sudo ssh git gcc g++ make tmux vim net-tools
+apt-get install -y libsm6 libxext6 libxrender-dev
+# wget https://mirrors.tuna.tsinghua.edu.cn/anaconda/archive/Anaconda3-2021.11-Linux-x86_64.sh
 
-# apt-get install -y python3-pip
+apt-get install -y python3-pip
 
-# pip3 install -r /tmp/requirements.txt
+pip3 install -r /tmp/requirements.txt
 
 ###
 # pip install pytorch or tensorflow here~
