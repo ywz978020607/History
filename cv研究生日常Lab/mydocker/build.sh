53f5838cc09b7005349cd4fcd80b32ac97086218
ywz978020607 <ywzsunny@buaa.edu.cn>
2022-04-18 20:26:17 +0800

    up

--- cv研究生日常Lab/mydocker/build.sh
+++ cv研究生日常Lab/mydocker/build.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
-apt-get update
+#换源
+sed -i s@/archive.ubuntu.com/@/mirrors.aliyun.com/@g /etc/apt/sources.list
+sed -i s@/security.ubuntu.com/@/mirrors.aliyun.com/@g /etc/apt/sources.list
+apt clean
+apt-get update -y
+
 apt-get install -y sudo ssh git gcc g++ make tmux vim net-tools
 apt-get install -y libsm6 libxext6 libxrender-dev
 # wget https://mirrors.tuna.tsinghua.edu.cn/anaconda/archive/Anaconda3-2021.11-Linux-x86_64.sh
