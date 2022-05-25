64a711a0b726f5e95a2942742828774be9026146
ywz978020607 <ywzsunny@buaa.edu.cn>
2022-05-25 21:30:47 +0800

    up

--- web相关 (python全栈系列)/web备份/django备份/可登录注册 备份 django Mongodb 用户注册登陆改密操作(session用sqlite3)/django1/app1/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('', views.index),  #第一次分应用，之前urls.py是include而已
-]
--- web相关 (python全栈系列)/web备份/django备份/可登录注册 备份 django Mongodb 用户注册登陆改密操作(session用sqlite3)/django1/user/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-	path('login.html', views.loginView, name='login'),
-	path('register.html', views.registerView, name='register'),
-	path('setpassword.html', views.setpasswordView, name='setpassword'),
-	path('logout.html', views.logoutView, name='logout'),
-]
