601a8961117244d28120b285caee154226845f00
Sharklet <32101163+ywz978020607@users.noreply.github.com>
2022-10-31 15:18:08 +0800

    Create autogit.sh

---
+++ cv研究生日常Lab/autogit.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# . autogit.sh
+
+# 获取更新当前分支变量
+# temp_branch=$(git symbolic-ref -q --short HEAD)
+# echo $temp_branch
+
+# 自动更新主分支并合并到本分支
+alias automerge="temp_branch=$(git symbolic-ref -q --short HEAD) && git checkout master && git pull origin master && git checkout $temp_branch && git merge master && git branch | grep '*'"
+
+#
+alias gaddup="git add -A && git commit -m 'up'"
+alias gpush="temp_branch=$(git symbolic-ref -q --short HEAD) && git push origin $temp_branch"
+
+
