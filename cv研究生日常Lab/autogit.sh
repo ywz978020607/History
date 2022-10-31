b5e739e4f08d35c8873f0f8d0f5c952cfba30427
Sharklet <32101163+ywz978020607@users.noreply.github.com>
2022-10-31 15:53:37 +0800

    Update autogit.sh

--- cv研究生日常Lab/autogit.sh
+++ cv研究生日常Lab/autogit.sh
@@ -6,10 +6,15 @@
 # echo $temp_branch
 
 # 自动更新主分支并合并到本分支
-alias gmerge="temp_branch=$(git symbolic-ref -q --short HEAD) && git checkout master && git pull origin master && git checkout $temp_branch && git merge master && git branch | grep '*'"
+gmerge(){
+    temp_branch=$(git symbolic-ref -q --short HEAD) && echo $temp_branch && git checkout master && git pull origin master && git checkout $temp_branch && git merge master && echo $temp_branch;
+}
 
-#
+# 自动推送到远程本分支
+gpush(){
+    temp_branch=$(git symbolic-ref -q --short HEAD) && echo $temp_branch && git push origin $temp_branch
+}
+
 alias gaddup="git add -A && git commit -m 'up'"
-alias gpush="temp_branch=$(git symbolic-ref -q --short HEAD) && git push origin $temp_branch"
 
-
+# 注：如果使用alias，对于$temp_branch无法动态更新，需要重新alias，使用函数代替
